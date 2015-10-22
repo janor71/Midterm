c7e2cefe1e7cf31efdc57f5b441cfd04fbc688f3
Rebeca <reca985@yahoo.com.ar>
2015-10-21 21:57:49 -0400

    Update m1janowicz.java

--- m1janowicz.java
+++ m1janowicz.java
@@ -1,4 +1,4 @@
-// MIDTERM M1 
+// MIDTERM M1 10/21/15
 
 // MY FIRST NAME IS ------ Rebeca
 // MY LAST NAME IS --------Janowicz
@@ -16,13 +16,13 @@ float rootX, rootY,  rootDX, rootDY;
 float jamX,  jamY,  jamDX,  jamDY; 
 float voiceX,  voiceY,  voiceDX,  voiceDY; 
 
-String title=  "CST112 MIDTERM EXAM";
-String news=   "Click any root to reset it to right half of table.  (r resets all.)";
+String title=  "CST112 MIDTERM EXAM" ;
+String news=   "Click any ball to reset it to right half of table.  (r resets all.)";
 String author=  "Rebeca Janowicz";
 
-float left, right, up, down;     // TABLE PERIMETER 
+float left, right, up, down;                           // TABLE PERIMETER 
 float middle= 250;
-boolean wall = true, all false, mickey= false;
+//boolean wall = true, all false, mickey= false;;    // struggling with this code 
 int pooltableR=23, pooltableG=85, pooltableB= 18;     // GREEN POOL TABLE 
 
 // SETUP
@@ -36,23 +36,25 @@ void setup(){
    reset();
 }
 void reset() {
+  
 //LOCATION OF BALLS
-     rootX =  random(middle,left);        rootY= random(up,down);
-     jamX =   random(middle,left);        jamY = random(up,down);
-     voiceX = random(middle,left);        voiceY =random(up,down);
+     rootX =  random(middle,right);        rootY= random(up,down);
+     jamX =   random(middle,right);        jamY = random(up,down);
+     voiceX = random(middle,right);        voiceY =random(up,down);
      
 // SPEED OF BALLS
-     rootDX =  random(1,4);        rootDY= random(1,4);
-     jamDX =   random(1,4);        jamDY = random(1,4);
-     voiceDX = random(1,4);        voiceDY=random(1,4);
+     rootDX =  random(1,2);        rootDY= random(1,2);
+     jamDX =   random(1,2);        jamDY = random(1,2);
+     voiceDX = random(1,2);        voiceDY=random(1,2);
 }
-  
   
   
 void draw() {
       poolTable(left, right, up, down);
       bounce();
       balls();
+      collisions();
+      message();
 }     
 
 // SHOWING POOL TABLE      
@@ -66,25 +68,25 @@ void poolTable(float left, float right, float up, float down){
       rect(left,right,up+100,down+50); 
   }  
   
-//  WALL AT MIDDLE OF THE TABLE 
-  if (wall) {
-    float middle=  (right+left)/2;    
+// WALL AT MIDDLE OF THE TABLE 
+ /* if (wall) { 
+    float middle=  (right+left)/2;                // still working on it. 
     stroke( 0, 127, 0 );
     line( middle,up+10, middle,down-10 );
   }
  //stroke(0);
  // strokeWeight(1);
-}
+*/
   
 // SHOW BALLS 
   void balls() {
       noStroke();
       fill(252,231,66);                      // YELLOW root
-      ellipse( rootX,rootY,40,40);
+      ellipse( rootX,rootY,30,30);
       fill(27,31,170 );                      // BLUE root
-      ellipse( jamX,jamY, 40,40 );
+      ellipse( jamX,jamY, 30,30 );
       fill(240,52,19);                       // RED root
-      ellipse( voiceX,voiceY, 40,40 ); 
+      ellipse( voiceX,voiceY, 30,30 ); 
      
 // BALLS NUMBERED      
       fill(0); 
@@ -96,12 +98,42 @@ void poolTable(float left, float right, float up, float down){
 //BALLS BOUNCE OF POOL TABLE  
   void bounce() {
     
-      rootX += rootDX; if ( rootX < left || rootX > right )  rootDX *= -1; 
+      rootX += rootDX; if ( rootX > middle|| rootX < right )  rootDX *= -1; 
       rootY += rootDY; if ( rootY < up || rootY > down)      rootDY *=  -1;
       
-      jamX += jamDX;   if ( jamX < left || jamX > right )    jamDX *= -1;
+      jamX += jamDX;   if ( jamX < middle || jamX > right )    jamDX *= -1;
       jamY += jamDY;   if ( jamY < up || jamY > down)        jamDY *=  -1;
        
-      voiceX += voiceDX;  if ( voiceX < left || voiceX  > right) voiceDX *= -1;
+      voiceX += voiceDX;  if ( voiceX < middle || voiceX  > right) voiceDX *= -1;
       voiceY += voiceDY;  if ( voiceY < up || voiceY > down)     voiceDY *=  -1;  
 }
+void collisions() {
+      float tmp;
+      
+        if ( dist( rootX,rootY, jamX,jamY ) < 30 ) {
+          tmp=jamDX;  jamDX=rootDX;  rootDX=tmp;
+          tmp=jamDY;  jamDY=rootDY;  rootDY=tmp;
+        }
+          
+          if ( dist( jamX,jamY, voiceX,voiceY ) < 30 ) {
+          tmp=voiceDX;  voiceDX=jamDX;  jamDX=tmp;
+          tmp=voiceDY;  voiceDY= jamDY;  jamDY=tmp;
+        }
+           if ( dist( rootX,rootY, voiceX,voiceY ) < 30 ) {
+          tmp=voiceDX;  voiceDX=rootDX;  rootDX=tmp;
+          tmp=voiceDY;  voiceDY= rootDY;  rootDY=tmp;
+         }
+  }
+
+  void message() {
+      fill(0);
+      stroke(0);
+      text( title, 220, 20 );
+      text( news, 320, 40);
+      text( author,320, 60);
+  }
+  void keyPressed() {
+    
+     if (key == 'p') background(237,121,255);
+     
+  }
